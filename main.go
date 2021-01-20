97956355f017570743a85ef7e64c319f3f6ff786
yykhomenko <yykhomenko@gmail.com>
2021-01-20 02:34:58 +0200

    added print error

--- main.go
+++ main.go
@@ -1,6 +1,10 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+)
 
 func main() {
 	app := fiber.New()
@@ -9,5 +13,5 @@ func main() {
 		return c.SendString("Hello, World!")
 	})
 
-	app.Listen(":80")
+	log.Fatal(app.Listen(":8080"))
 }
